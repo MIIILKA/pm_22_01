346e4dda625d7c2163137db39d079070f9bb9ca2
Mykola Baliuk <baliukschool@gmail.com>
2024-09-11 20:44:26 +0300

    працює gulp але html+css, але scss зараз не бачить і не вносить зміни

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,72 @@
 import gulp from 'gulp';
 import gulpSass from 'gulp-sass';
-import dartSass from 'sass';
+import * as sass from 'sass'; // Updated import statement
 import browserSync from 'browser-sync';
 import uglify from 'gulp-uglify';
 import imagemin from 'gulp-imagemin';
 import cleanCSS from 'gulp-clean-css';
 
-const sass = gulpSass(dartSass);
+const sassCompiler = gulpSass(sass); // Rename to avoid confusion
 const sync = browserSync.create();
 
-// Ваші задачі тут
+// SCSS Compilation Task
 export const styles = () => {
-    return gulp.src('./app/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('./app/css/'))
-        .pipe(sync.stream());
+    return gulp.src('./my-project/app/sass/**/*.scss') // Take all SCSS files
+        .pipe(sassCompiler().on('error', sassCompiler.logError)) // Compile SCSS to CSS
+        .pipe(cleanCSS()) // Minify the compiled CSS
+        .pipe(gulp.dest('./my-project/dist/css/')) // Save to dist/css
+        .pipe(sync.stream()); // Auto-refresh browser
 };
 
+// CSS Minification Task
+export const stylescss = () => {
+    return gulp.src('./my-project/app/css/**/*.css') // Take all CSS files
+        .pipe(cleanCSS()) // Minify the CSS
+        .pipe(gulp.dest('./my-project/dist/css/')) // Save to dist/css
+        .pipe(sync.stream()); // Auto-refresh browser
+};
+
+// Image Optimization Task
 export const images = () => {
-    return gulp.src('./app/img/**/*')
+    return gulp.src('./my-project/app/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/img/'))
+        .pipe(gulp.dest('./my-project/dist/img/'))
         .pipe(sync.stream());
 };
 
+// HTML Copy Task
 export const html = () => {
     return gulp.src('./my-project/app/*.html')
         .pipe(gulp.dest('./my-project/dist'))
         .pipe(sync.stream());
 };
 
-
-
+// JavaScript Minification Task
 export const scripts = () => {
-    return gulp.src('./app/js/**/*.js')
+    return gulp.src('./my-project/app/js/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js/'))
+        .pipe(gulp.dest('./my-project/dist/js/'))
         .pipe(sync.stream());
 };
 
+// Watch Files for Changes
 export const watchFiles = () => {
-    gulp.watch('./app/index.html', html);
-    gulp.watch('./app/sass/**/*.scss', styles);
-    gulp.watch('./app/js/**/*.js', scripts);
-    gulp.watch('./app/img/**/*', images);
+    gulp.watch('./my-project/app/index.html', html);
+    gulp.watch('./my-project/app/sass/**/*.scss', styles); // Watch SCSS files
+    gulp.watch('./my-project/app/css/**/*.css', stylescss); // Watch CSS files
+    gulp.watch('./my-project/app/js/**/*.js', scripts);
+    gulp.watch('./my-project/app/img/**/*', images);
 };
 
+// Serve and Live Reload
 export const serve = () => {
     sync.init({
         server: {
-            baseDir: './my-project/dist',
+            baseDir: './my-project/dist', // Serve from dist folder
         },
         port: 3000,
     });
 };
 
-
-export default gulp.series(html, styles, scripts, images, serve, watchFiles);
+// Default Task
+export default gulp.series(html, styles, stylescss, scripts, images, serve, watchFiles);
